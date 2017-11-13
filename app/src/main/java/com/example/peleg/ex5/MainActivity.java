b94650779c3f7dc2e4262aaec90a1f924ac2a8cc
pelegdavidson <pelegd13@gmail.com>
2017-11-13 17:56:55 +0200

    EX5

--- app/src/main/java/com/example/peleg/ex5/MainActivity.java
+++ app/src/main/java/com/example/peleg/ex5/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.peleg.ex5;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout rl1 = (RelativeLayout) findViewById(R.id.rl);
+        TextView tvIntro = (TextView) findViewById(R.id.tvIntro);
+        LinearLayout ll1 = new LinearLayout(this);
+        ll1.setId(R.id.ll1Id);
+        ll1.setBackgroundResource(R.drawable.roundcolorborders);
+        ll1.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        llp.setMargins();
+        rl1.addView();
+
     }
 }
